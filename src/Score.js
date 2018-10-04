611d663a043cdd3a00337cbba4e178a5c7e3c195
github username <github email address>
2018-10-04 11:27:25 -0400

    added pictures

--- src/Score.js
+++ src/Score.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import jessbad from './images/jessbad.jpg'
+import jessmedium from './images/jessmedium.jpg'
+import jessgood from './images/jessgood.jpg'
+
 
 function Score(props) {
 
   const displayResults = () => {
     if ((props.score/10 * 100) > 60) {
-      return "Excellent Job!!"
+      return <h1> Jess says Great Job! <img src={jessgood} alt="jess_good" width="200"/></h1>
     }
     else if ((props.score/10 * 100) > 30 && (props.score/10 * 100) < 70) {
-      return "Hmmmmmmmm you did okay...."
+      return <h1> Jess says You Did Okay <img src={jessmedium} alt="jess_medium" width="200"/></h1>
+
     }
     else if ((props.score/10 * 100) < 40) {
-      return "Great job said no one"
+      return <h1> Jess says Oops Not Good <img src={jessbad} alt="jess_bad" width="200"/></h1>
     }
     else {
       return null
